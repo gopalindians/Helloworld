9d7a87252598eaeb3591fb6a6f76cb6d48606155
gopalindians <!@#QWe345>
2015-09-21 03:43:48 +0530

    Initial commit

---
+++ Hello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gopalindians
+ * Date: 21-09-2015
+ * Time: 03:40
+ */
+namespace HelloWorld\Hello;
+
+class Hello{
+    public function  __construct()
+    {
+        echo "Hello";
+    }
+}
